6afe8b8317ece175c71ac78c1a147ae0071e65af
Eduardo Machado <adoreleon@gmail.com>
2019-03-25 19:27:18 -0300

    git init

---
+++ README.md
@@ -0,0 +1,3 @@
+## Nyan Cat Pipeline Protector
+
+Simple python code to watch some CI/CD build and call for the nyan cat to brace yourselfs
---
+++ main.py
@@ -0,0 +1,13 @@
+import configparser
+
+
+def load_config_file():
+    pass
+
+
+def pipeline_listner():
+    pass
+
+
+if __name__ == '__main__':
+    pipeline_listner()
